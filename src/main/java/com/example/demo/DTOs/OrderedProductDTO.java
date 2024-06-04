f07743f7ea596e8e570d61f3fd0fdc16b1162154
Niedzinek <maciekniedzinski@gmail.com>
2024-06-04 16:21:24 +0200

    Dodane serwisy dla purchase i customer, utworzone dto dla purchase, ordered product, product.

---
+++ src/main/java/com/example/demo/DTOs/OrderedProductDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderedProductDTO {
+
+
+    private double quantity;
+
+    private ProductDTO product;
+
+
+
+}
---
+++ src/main/java/com/example/demo/DTOs/ProductDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDTO {
+
+    private Integer id;
+
+
+}
